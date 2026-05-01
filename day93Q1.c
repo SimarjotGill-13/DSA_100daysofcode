06818505c457612a4acabf637b537e652a00ece6
Simarjot Kaur Gill <677vksgill.com@gmail.com>
2026-05-01 15:32:05 +0530

    Day 93

---
+++ day93Q1.c
@@ -0,0 +1,38 @@
+/* Problem: Sort an array using insertion sort.
+Stable sort. Good for nearly sorted arrays.
+*/
+#include <stdio.h>
+
+void insertionSort(int arr[], int n) {
+int i, j, key;
+
+for (i = 1; i < n; i++) {
+key = arr[i];
+j = i - 1;
+
+while (j >= 0 && arr[j] > key) {
+arr[j + 1] = arr[j];
+j--;
+}
+
+arr[j + 1] = key;
+}
+}
+
+void printArray(int arr[], int n) {
+int i;
+for (i = 0; i < n; i++)
+printf("%d ", arr[i]);
+}
+
+int main() {
+int arr[] = {5, 2, 9, 1, 5, 6};
+int n = sizeof(arr)/sizeof(arr[0]);
+
+insertionSort(arr, n);
+
+printf("Sorted array: ");
+printArray(arr, n);
+
+return 0;
+}
